04a354e8daa4e2daba92a7c114a96c35d01eb0a2
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-02 14:23:00 +0100

    return storedValue

--- src/hooks/UseLocalStorage.js
+++ src/hooks/UseLocalStorage.js
@@ -3,12 +3,12 @@ import React from 'react';
 const UseLocalStorage = (key, initialValue) => {
 
     const [storedValue, setStoredValue] = useState(() => {
-        
-    })
+        const item = window.localStorage.getItem(key);
+        return item 
+            ? JSON.parse(item) 
+            : initialValue;
 
-    return (
-        <div>
+    });
 
-        </div>
-    );
+    return [storedValue];
 }  
