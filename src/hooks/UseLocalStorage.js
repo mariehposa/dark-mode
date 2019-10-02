e9e9dd63f3b81195734f204a968ab52fd7a164de
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-02 14:09:31 +0100

    create storedValue state

--- src/hooks/UseLocalStorage.js
+++ src/hooks/UseLocalStorage.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 const UseLocalStorage = (key, initialValue) => {
+
+    const [storedValue, setStoredValue] = useState(() => {
+        
+    })
+
     return (
         <div>
 
